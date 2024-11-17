c4adc5f8a917df922f4c975a0e9aeafa547c98fa
Soliken <155744710+soliken1@users.noreply.github.com>
2024-11-17 10:53:43 +0800

    Update Route

--- src/app/resetPassword/[token]/page.tsx
+++ src/app/resetPassword/[token]/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const page = () => {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      test
+      Forgot Password View
     </div>
   );
 };
