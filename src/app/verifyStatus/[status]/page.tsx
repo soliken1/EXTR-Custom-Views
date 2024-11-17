3cdef2fe737bf5622e27ba66870dc6638ac3d281
Soliken <155744710+soliken1@users.noreply.github.com>
2024-11-17 14:06:57 +0800

    Update Status

--- src/app/verifyStatus/[status]/page.tsx
+++ src/app/verifyStatus/[status]/page.tsx
@@ -1,23 +1,12 @@
 "use client";
 import React from "react";
-import { useParams } from "next/navigation";
 
 const VerificationStatus = () => {
-  const { status } = useParams();
-
-  if (status === "success") {
-    return (
-      <div className="w-screen h-screen flex justify-center items-center text-xl text-green-400">
-        Email Successfully Verified
-      </div>
-    );
-  } else {
-    return (
-      <div className="w-screen h-screen flex justify-center items-center text-xl text-red-400">
-        Email Already Verified
-      </div>
-    );
-  }
+  return (
+    <div className="w-screen h-screen flex justify-center items-center text-2xl font-semibold text-green-400">
+      Email Successfully Verified
+    </div>
+  );
 };
 
 export default VerificationStatus;
