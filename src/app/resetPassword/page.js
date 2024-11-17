2eede4344f1043df1395c4cff0f60f2a4ef91c8c
Soliken <155744710+soliken1@users.noreply.github.com>
2024-11-17 10:36:58 +0800

    Update Initials

---
+++ src/app/resetPassword/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      test
+    </div>
+  );
+};
+
+export default page;
